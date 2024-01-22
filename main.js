9a0de466730ffa08d5255cdc8d5d6bca1a0d6eaa
OmegatheKing <110073349+OmegatheKing@users.noreply.github.com>
2024-01-22 16:19:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ console.error(error);
 } else{
 console.log(results);
 document.getElementById("result_object_name").innerHTML= results[0].label;
-document.getElementById("result_object_accuracy").innerHTML= results[0].confidence.toFixed(3)*100;
+(document.getElementById("result_object_accuracy").innerHTML= results[0].confidence.toFixed(3)*100'%');
 }
 }
 
